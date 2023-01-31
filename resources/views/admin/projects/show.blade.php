b5a1c1913e4f61a98c5f837c30e0c6756a565c4d
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-31 17:02:24 +0100

    refactoring

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,77 +5,57 @@
 @section('content')
 <div class="container mt-3">
   @if ($project->cover_image)
-  <div class="card" style="width: 18rem;">
-    <img src="{{asset('storage/'. $project->cover_image)}}" class="card-img-top" alt="{{$project->title}}">
-    <div class="card-body">
-      <h5 class="card-title">{{$project->title}}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">Progetto eseguito per:{{$project->client_name}}</h6>
-      <p class="card-text"> {{$project->description}}</p>
-      {{-- <p class="card-text">{{$project->type?->name ?: 'Nessun Tipologia'}}</p> --}}
-      <p class="card-text">
-        @if ($project->type)
-        Tipologia: <a href="{{route('admin.types.show', $project->type)}}">{{$project->type->name}}</a>
-        @else
-        Nessun Categoria
-        @endif
-      </p>
-      <p class="card-text">
-        @if ($project->technologies)
-          @foreach ($project->technologies as $technology)
-          <span class="badge rounded-pill text-bg-dark"><a href="#" class="text-white text-decoration-none">{{$technology->name}}</a></span>
-          
-          @endforeach
-        @else
-        Nessun Categoria
-        @endif
-      </p>
+    <div class="card" style="width: 18rem;">
+      <img src="{{asset('storage/'. $project->cover_image)}}" class="card-img-top" alt="{{$project->title}}">
+      <div class="card-body">
+        <h5 class="card-title">{{$project->title}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Progetto eseguito per:{{$project->client_name}}</h6>
+        <p class="card-text"> {{$project->description}}</p>
+        <p class="card-text">
+          @if ($project->type)
+          Tipologia: <a href="{{route('admin.types.show', $project->type)}}">{{$project->type->name}}</a>
+          @else
+          Nessun Categoria
+          @endif
+        </p>
+        <p class="card-text">
+          @if ($project->technologies)
+            @foreach ($project->technologies as $technology)
+            <span class="badge rounded-pill text-bg-dark"><a href="#" class="text-white text-decoration-none">{{$technology->name}}</a></span>
             
-      <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Go Back</a>
+            @endforeach
+          @else
+          Nessun Categoria
+          @endif
+        </p>
+        <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Go Back</a>
+      </div>
     </div>
-  </div>
-  
-  
-  
   @else
-  <div class="card" style="width: 18rem;">
-    {{-- @dd($project->type) --}}
-            <div class="card-body">
-              <h5 class="card-title">{{$project->title}}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Progetto eseguito per:{{$project->client_name}}</h6>
-              <p class="card-text">{{$project->description}}</p>
-              <p class="card-text">
-                @if ($project->type)
-                Tipologia: <a href="{{route('admin.types.show', $project->type)}}">{{$project->type->name}}</a>
-                @else
-                Nessun Categoria
-                @endif
-              </p>
-              <p class="card-text">
-                @if ($project->technologies)
-                  @foreach ($project->technologies as $technology)
-                  <span class="badge rounded-pill text-bg-dark"><a href="#" class="text-white text-decoration-none">{{$technology->name}}</a></span>
-                  
-                  @endforeach
-                @else
-                Nessun Categoria
-                @endif
-              </p>
-              <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Go Back</a>
-            </div>
-          </div>
-        @endif
-                
-                
-                
-              
-              
-
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">{{$project->title}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Progetto eseguito per:{{$project->client_name}}</h6>
+        <p class="card-text">{{$project->description}}</p>
+        <p class="card-text">
+          @if ($project->type)
+          Tipologia: <a href="{{route('admin.types.show', $project->type)}}">{{$project->type->name}}</a>
+          @else
+          Nessun Categoria
+          @endif
+        </p>
+        <p class="card-text">
+          @if ($project->technologies)
+            @foreach ($project->technologies as $technology)
+            <span class="badge rounded-pill text-bg-dark"><a href="#" class="text-white text-decoration-none">{{$technology->name}}</a></span>
             
-        {{-- <h1>{{$project->title}}</h1>
-        <h3>Progetto eseguito per: {{$project->client_name}}</h3>
-        <img src="{{asset('storage/'. $project->cover_image)}}" alt="{{$project->title}}">
-        <p class="mt-3">
-            {{$project->description}}
-        </p> --}}
+            @endforeach
+          @else
+          Nessun Categoria
+          @endif
+        </p>
+        <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Go Back</a>
+      </div>
     </div>
-@endsection
+  @endif
+@endsection    
